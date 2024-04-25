a0e788ec22d2f07c34ac9cf8d8dfb5a1f2e072ad
Alexey Prohorov <a.prokhorov@revizto.com>
2024-04-25 13:36:26 +0300

    Added the ability to extend StaticDriver for greater flexibility.

--- src/DAMA/DoctrineTestBundle/Doctrine/DBAL/StaticDriver.php
+++ src/DAMA/DoctrineTestBundle/Doctrine/DBAL/StaticDriver.php
@@ -28,14 +28,14 @@ class StaticDriver extends Driver\Middleware\AbstractDriverMiddleware
             return parent::connect($params);
         }
 
-        $key = sha1(json_encode($params));
+        $key = $this->getConnectionHash($params);
 
-        if (!isset(self::$connections[$key])) {
-            self::$connections[$key] = parent::connect($params);
-            self::$connections[$key]->beginTransaction();
+        $connection = $this->getConnection($key);
+        if (null === $connection) {
+            $connection = parent::connect($params);
+            $this->addConnection($key, $connection);
+            $connection->beginTransaction();
         }
-
-        $connection = self::$connections[$key];
 
         $platform = $this->getPlatform($connection, $params);
 
@@ -75,6 +75,21 @@ class StaticDriver extends Driver\Middleware\AbstractDriverMiddleware
         foreach (self::$connections as $connection) {
             $connection->commit();
         }
+    }
+
+    protected function getConnectionHash(array $params): string
+    {
+        return sha1(json_encode($params));
+    }
+
+    protected function getConnection(string $key): ?Connection
+    {
+        return self::$connections[$key] ?? null;
+    }
+
+    protected function addConnection(string $key, Connection $connection): void
+    {
+        self::$connections[$key] = $connection;
     }
 
     private function getPlatform(Connection $connection, array $params): AbstractPlatform
