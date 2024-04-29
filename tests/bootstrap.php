67ba0f13142112b7fdb7561e4e50e78a827f467e
Ashvini Saxena <ashvinisaxena77@gmail.com>
2024-04-29 13:42:23 +0530

    Update bootstrap.php
    First we check if the migrations are upto the point
    if not we drop the database if it exists
    create
    migrate

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,37 +1,52 @@
 <?php
 
+use App\Kernel;
 use Doctrine\Deprecations\Deprecation;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Component\Console\Input\ArrayInput;
-use Tests\Functional\app\AppKernel;
+use Symfony\Component\Dotenv\Dotenv;
 
-require_once __DIR__.'/../vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+}
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+}
 
 if (class_exists(Deprecation::class)) {
     Deprecation::enableWithTriggerError();
 }
 
+bootstrap();
 function bootstrap(): void
 {
-    $kernel = new AppKernel('test', true);
+    $kernel = new Kernel('test', true);
     $kernel->boot();
 
     $application = new Application($kernel);
     $application->setCatchExceptions(false);
     $application->setAutoExit(false);
 
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:drop',
-        '--if-exists' => '1',
-        '--force' => '1',
-    ]));
+        // check if existing DB is up-to-date
+        // if not drop it, recreate and migrate
+        $output = $application->run(new ArrayInput(['command' => 'doctrine:migrations:up-to-date', '--no-interaction' => true]));
 
-    $application->run(new ArrayInput([
-        'command' => 'doctrine:database:create',
-    ]));
+        // some error
+        if ($output == 1) loadDatabaseAndMigrate($application);
 
-    $kernel->getContainer()->get('doctrine')->getConnection()->executeQuery('CREATE TABLE test (test VARCHAR(10))');
+
     $kernel->shutdown();
 }
 
-bootstrap();
+function loadDatabaseAndMigrate($application): void
+{
+    $application->run(new ArrayInput(['command' => 'doctrine:database:drop', '--if-exists' => '1', '--force' => '1',]));
+
+    $application->run(new ArrayInput(['command' => 'doctrine:database:create', '--no-interaction' => true]));
+
+    $application->run(new ArrayInput(['command' => 'doctrine:migrations:migrate', '--no-interaction' => true]));
+
+}
