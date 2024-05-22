15c6c92ef0a4e3ea8dc605d88d872459d413dca6
David Maicher <mail@dmaicher.de>
2024-05-22 09:50:09 +0200

    fix deprecation for Extension class

--- src/DAMA/DoctrineTestBundle/DependencyInjection/DAMADoctrineTestExtension.php
+++ src/DAMA/DoctrineTestBundle/DependencyInjection/DAMADoctrineTestExtension.php
@@ -3,7 +3,7 @@
 namespace DAMA\DoctrineTestBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 class DAMADoctrineTestExtension extends Extension
 {
